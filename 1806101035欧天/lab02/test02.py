89943a012404cca1f827a0e28a292f8e52f2250e
wxjot <467108798@qq.com>
2019-04-09 12:53:06 +0800

    第一次提交代码

--- 1806101035欧天/lab02/test02.py
+++ 1806101035欧天/lab02/test02.py
@@ -1,3 +1,30 @@
+#{[()]
+def check
+str='{[()]}'
 stack=[]
-for i in ("{[()]}"):
-    stack.append(i)
+zuobian='{[('
+youbian='}])'
+#1.依次遍历每一个字符
+for i in str:
+    print(i)
+
+#2.如果是左括号，入栈
+    if i in zuobian:
+        stack.append(i)
+    elif i in youbian:
+        stack.append(i)
+    if stack and stack[-1] == str[i]:
+        stack.pop()
+    else:
+
+
+#3.如果是右括号
+#  出栈并且与当前左括号进行匹配
+#  如果匹配则继续
+#4.当所有字符都处理完成后
+#  栈不为空，返回False
+#  如果栈为空，则return True
+
+
+
+
