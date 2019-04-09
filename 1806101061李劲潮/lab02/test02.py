d8760b1efa22f33dcf176a856516be65d61ecb6e
LeeJinchao <1224163781@qq.com>
2019-04-09 11:27:56 +0800

    括号匹配

--- 1806101061李劲潮/lab02/test02.py
+++ 1806101061李劲潮/lab02/test02.py
@@ -1,28 +1,31 @@
-def parCheker(str):
-    s = Stack()
-    balanced = True
-    index = 0
-    while index<len(str) and balanced:
-        str1 = str[index]
-        if str1 in '([{':
-            s.push(str1)
+#定义匹配括号函数
+def match_parentheses(s):
+    #定义栈
+    stack = []
+    parentheses = "()[]{}"
+    #判断是否为括号
+    for i in range(len(s)):
+        si = s[i]
+        if parentheses.find(si) == -1:
+            continue
+        #是括号就压入
+        if si == '(' or si == '[' or si == '{':
+            stack.append(si)
+            continue
+        if len(stack) == 0:
+            return False
+        p = stack.pop()
+        if (p == '(' and si == ')') or (p == '[' and si == ']') or (p == '{' and si == '}'):
+            continue
         else:
-            if s.isEmpty():
-                balanced = False
-            else:
-                top = s.pop()
-                if not matches(top,str1):
-                    balanced = False
-
-        index += 1
-
-    if s.isEmpty() and balanced:
-        return True
-    else:
+            return False
+    if len(stack) > 0:
         return False
-
-
-def matches(open,close):
-    opens = '([{'
-    closes = ')]}'
-    return opens.index(open) == closes.index(close)
+    return True
+if __name__ == '__main__':
+    s = "{abc}{de}(f)[(g)"
+    result = match_parentheses(s)
+    print(s, result)
+    s = "0{abc}{de}(f)[(g)]9"
+    result = match_parentheses(s)
+    print(s, result)
