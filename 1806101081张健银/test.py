1b218ece2537fbe176effc69391e4f0783aba1c9
彭彦乐 <1419512991@qq.com>
2019-04-09 10:09:48 +0800

    第一次提交

--- 1806101081张健银/test.py
+++ 1806101081张健银/test.py
@@ -1,5 +1,8 @@
-stack = []
-for i in 'Hello,World':
+stack=[]
+str1="Hello,world"
+for i in "Hello,world!":
     stack.append(i)
+result = []
 while len(stack) is not 0:
-    print(stack.pop())
+    result.append(stack.pop())
+print("".join(result))
