d9aa517fba834b7746c1ca911d708fa0ff7fd7fb
靳杨 <271536112@qq.com>
2019-04-12 11:46:09 +0800

    字符串反转

---
+++ 1806101041靳杨/day20190412/stack.py
@@ -0,0 +1,29 @@
+#stack=[]
+
+#压栈append
+
+#for i in range(1,11):
+    #stack.append(i)
+
+
+#ctrl+D 复制并粘贴所选内容
+#ctrl+X 删除
+
+#a=stack.pop()
+#print(a)
+#for i in  'hello':
+    #print(i)
+#while len(stack) is not 0:
+    #a=stack.pop()
+    #print(a)
+
+
+stack=[]
+a='hello world!'
+for i in a:
+    stack.append(i)
+
+while len(stack) is not 0:
+    print(stack.pop())
+
+
