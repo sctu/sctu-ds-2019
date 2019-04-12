e111f8a9b97532db5bf99b96206b3f12b235b804
chengongsuo <2547253080@qq.com>
2019-04-12 11:14:07 +0800

    练习代码提交和更新。

--- 1001陈功锁/day20190412/stack.py
+++ 1001陈功锁/day20190412/stack.py
@@ -17,7 +17,19 @@ for i in range(1, 11):
 
 # ctrl + d
 # ctrl + x
-a = stack.pop()
-print(a)
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
 
+# 代码重复
 
+# 当栈不为空的时候，一直pop()
+while len(stack) is not 0:
+    a = stack.pop()
+    print(a)
