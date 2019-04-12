c6300958e01e0d72869ec9ee87d46c4e348ee680
luoling00 <202536850@qq.com>
2019-04-12 11:03:26 +0800

    压栈

---
+++ 1806101080雒铃/DAY20190412/STACK.py
@@ -0,0 +1,17 @@
+stack=[]
+#压栈
+stack.append(1)
+stack.append(2)
+stack.append(3)
+#crtl+d 复制
+#crtl+x 粘贴
+a=stack.pop ()
+print(a)
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+#...
+stack.append(10)
+for i in range(1,11):
+    stack.append(i)
