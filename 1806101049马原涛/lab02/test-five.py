7bfac6479793a85878338f41d77d0ddc84bf9ce1
马原涛 <1176632450@qq.com>
2019-04-14 20:34:38 +0800

    回文判断

---
+++ 1806101049马原涛/lab02/test-five.py
@@ -0,0 +1,34 @@
+# 4、编程实现MinStack.
+stack=[]
+MinStack=[]
+minIndex=0
+def once(stack, MinStack, minIndex, shuru):
+    stack.append(shuru)
+    MinStack.append(shuru)
+    minIndex=shuru
+    return minIndex
+def Min(stack, MinStack,  minIndex,shuru):
+    minIndex=MinStack[-1]
+    if shuru < minIndex:
+        stack.append(shuru)
+        minIndex = shuru
+
+        MinStack.append(minIndex)
+    else:
+        stack.append(shuru)
+        MinStack.append(minIndex)
+
+while True:
+    shuru = eval(input('请输入一个值压栈：'))
+    if len(stack)==0:
+        once(stack, MinStack, minIndex, shuru)
+    else:
+        Min(stack, MinStack, minIndex, shuru)
+    print('最小值为%d'%MinStack[-1])
+    # print(stack,MinStack)
+
+
+
+
+
+
