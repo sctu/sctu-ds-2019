1ead37ab7120741e2ec30aea440c4ba1ca779cf8
DengSnowMountain <2576468332>
2019-04-15 23:30:32 +0800

    abcdcba

---
+++ 1806101001邓雪峰/lab02/huiwenpanduan.py
@@ -0,0 +1,14 @@
+b='abcdcba'
+stack = []
+for i in b:
+    stack.append(i)
+
+p = []
+while len(stack) is not 0:
+    p.append((stack.pop()))
+
+a=''.join(p)
+if a==b:
+    print(True)
+else:
+    print(False)
