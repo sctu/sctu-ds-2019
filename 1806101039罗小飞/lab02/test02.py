6f698447c20b61b512ecede4272149759c5e638a
luoxiaofeizhensao <1397141894@qq.com>
2019-04-15 16:36:18 +0800

    判断括号是否匹配

--- 1806101039罗小飞/lab02/test02.py
+++ 1806101039罗小飞/lab02/test02.py
@@ -1,29 +1,29 @@
 stackl=[]
 stackr=[]
-left='([{'
-right=')]}'
-s=input('请输入判断语句：')
-for i in s:
-    if i==left:
-        stackl.append(i)
-        changdul=len(stackl)
-    elif i==right:
-        stackr.append(i)
-        changdur=len(stackr)
-        if changdul<changdur:
-            print("不匹配")
-            break
+stackr2=[]
+left=['(','[','{']
+right=[')',']','}']
+a=input('请输入判断括号 (若输出多个结果视第一个输出结果为有效！):')
+if a[0] in right:
+    print("不匹配")
+else:
+    for i in a:
+        if i=='(' or i=='[' or i=='{':
+            stackl.append(i)
+        elif i==')' or i==']' or i=='}':
+            stackr.append(i)
+            while len(stackl)<len(stackr):
+                print('不匹配')
         else:
             continue
+    if len(stackl)==len(stackr):
+        stackr2.append(stackr.pop())
+        chuzhanl=stackl.pop()
+        chuzhanr2=stackr2.pop()
+        for q in chuzhanl:
+                z=left.index(q)
+        for w in chuzhanr2:
+                x=right.index(w)
+        print("不匹配") if z==x else print("匹配")
     else:
-        continue
-print(stackl.pop())
-print(stackr.pop())
-if chuzhanl == '( 'and chuzhanr==')'  or chuzhanl == '[' and chuzhanr == ']':
-    print('匹配')
-else:
-    print('不匹配')
-if chuzhanl == '{'and chuzhanr=='}':
-    print('匹配')
-else:
-    print('不匹配')
+        print('不匹配')
