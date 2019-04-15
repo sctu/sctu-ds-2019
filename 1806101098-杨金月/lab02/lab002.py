b7f13934f33476ec486ffda000675376f2c2c028
chengcaijason <2608831274@qq.com>
2019-04-15 18:13:46 +0800

    实验二（2）题

--- 1806101098-杨金月/lab02/lab002.py
+++ 1806101098-杨金月/lab02/lab002.py
@@ -6,8 +6,10 @@ R = []
 for i in str:
     if i in Left:
         stack.append(i)
+        print(i)
     else:
         R = stack.pop()
+        print(R)
         right_index = Right.index(R)
         left_index = Left.index(i)
         if left_index != right_index:
@@ -17,6 +19,8 @@ if len(stack) is 0:
     print('True')
 else:
     print('Wrong')
+
+
 
 
 
