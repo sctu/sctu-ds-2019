e48bf05c33616161aebe0d59f323ff26672bfbac
chengongsuo <2547253080@qq.com>
2019-04-15 11:29:31 +0800

    练习代码提交和更新。

--- 1001陈功锁/day20190415/test02.py
+++ 1001陈功锁/day20190415/test02.py
@@ -16,7 +16,10 @@ for i in '{[()]}':
         #print(i)
         left_parentheses = stack.pop()
 
+        # 当前左括号下标
         left_index = LEFT.index(left_parentheses)
+
+        # 当前右括号下标
         right_index = RIGHT.index(i)
 
         if left_index != right_index:
