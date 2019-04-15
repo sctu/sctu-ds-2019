4c2ef8b79de9c79d5f59572392c2480eb201e5c5
wangyongcai1 <1464792341@qq.com>
2019-04-15 16:24:05 +0800

    test02

---
+++ 1806101045王永才/lab02/test02.py
@@ -0,0 +1,20 @@
+SYMBOLS = {'}': '{', ']': '[', ')': '(', '>': '<'}
+SYMBOLS_L, SYMBOLS_R = SYMBOLS.values(), SYMBOLS.keys()
+
+def check(s):
+    arr = []
+    for c in s:
+        if c in SYMBOLS_L:
+            # 左符号入栈
+            arr.append(c)
+        elif c in SYMBOLS_R:
+            # 右符号要么出栈，要么匹配失败
+            if arr and arr[-1] == SYMBOLS[c]:
+                arr.pop()
+            else:
+                return False
+
+    return True
+
+print(check("3 * {3 +[(2 -3) * (4+5)]}"))
+print(check("3 * {3+ [4 - 6}]"))
