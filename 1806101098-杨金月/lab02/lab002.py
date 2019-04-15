ac7d272aaa6e338169c229a578fba7a720c1332e
chengcaijason <2608831274@qq.com>
2019-04-15 17:37:21 +0800

    实验二（1）题

---
+++ 1806101098-杨金月/lab02/lab002.py
@@ -0,0 +1,22 @@
+str = "{[()]}"
+stack = []
+Left = "{{("
+Right = "})}"
+parens1 = []
+for ch in str:
+    if ch  in Left:
+        stack.append(ch)
+        print(ch)
+    else:
+       ch2=ch
+       print(ch2)
+if ch_len = ch2_len:
+    print("True")
+else:
+    print('Wrong')
+
+
+
+
+
+
