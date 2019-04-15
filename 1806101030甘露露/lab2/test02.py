6fa478e30192a0b1be2151cb6982193f4b4d2357
ganlulu <291500103@qq.com>
2019-04-15 17:39:34 +0800

    括号匹配

--- 1806101030甘露露/lab2/test02.py
+++ 1806101030甘露露/lab2/test02.py
@@ -1,15 +1,15 @@
-st = []
+stack = []
 friends = input("请输入:")
 for i in friends:
     if i=="("and"{"and"[":
-        st.append(i)
-    if i==")"and st[-1]=="(":
-        st.pop()
-    if i=="}"and st[-1]=="{":
-        st.pop()
-    if i=="]"and st[-1]=="[":
-        st.pop()
-if len(st)==0:
+        stack.append(i)
+    if i==")"and stack[-1]=="(":
+        stack.pop()
+    if i=="}"and stack[-1]=="{":
+        stack.pop()
+    if i=="]"and stack[-1]=="[":
+        stack.pop()
+if len(stack)==0:
     print("True")
 else:
     print("Flase")
