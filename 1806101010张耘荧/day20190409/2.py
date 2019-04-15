e363da6b12334604e1a1e50d92c0a0c817911e42
zhangyunying <1607884857@qq.com>
2019-04-15 23:45:09 +0800

    day20190409-2

--- 1806101010张耘荧/day20190409/2.py
+++ 1806101010张耘荧/day20190409/2.py
@@ -1,13 +1,33 @@
-num=input("请输入括号：")
-stack=[]
+num = input("请输入括号：")
+stack = []
 for i in num:
     if i is "("or\
             i is "["or\
             i is "{":
         stack.append(i)
-    #elif i==")"or"]"or"}":
-    #stack.pop()
+    elif i == ")":
+        s = stack.pop()
+        if s == "(":
+            continue
+        else:
+            print("不匹配")
+    elif i == "]":
+        s=stack.pop()
+        if s == "[":
+            continue
+        else:
+            print("不匹配")
+    elif i == "}":
+        s=stack.pop()
+        if s == "{":
+            continue
+        else:
+            print("不匹配")
+if len(stack)%2!=0:
+    print("不匹配")
+else:
+    print("匹配")
 
 
-print(stack.pop())
-print(stack.pop())
+
+
