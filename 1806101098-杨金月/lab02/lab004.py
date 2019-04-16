ccd206e7816c0dab04daf6ddca236e2cf037cd9c
chengcaijason <2608831274@qq.com>
2019-04-16 09:07:00 +0800

    实验二（4）题

--- 1806101098-杨金月/lab02/lab004.py
+++ 1806101098-杨金月/lab02/lab004.py
@@ -1,17 +1,13 @@
 arr=[]
 l=[]
-#入栈，将第一个全部数分别压入两个栈中
 while True:
     n=eval(input())
     arr.append(n)
     if len(l)==0:
         l.append(n)
-#之后入栈，直接压入arr，列表l中判断前一个是否大于后一个
-#如果前者大则返回最小值为后者，且后者压栈到列表l
     else:
         if l[-1]>=n:
             l.append(n)
-# 如果前者大则返回最小值为前者，且将最小值压栈
         else:
             x=l[-1]
             l.append(x)
