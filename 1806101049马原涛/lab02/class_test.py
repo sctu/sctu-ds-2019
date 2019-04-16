bf49d62291acee35cc1d49d1b53984eb747b861f
马原涛 <1176632450@qq.com>
2019-04-16 11:21:01 +0800

    类的练习

---
+++ 1806101049马原涛/lab02/class_test.py
@@ -0,0 +1,13 @@
+class information:
+    def __init__(self,name,age,gender,sort):
+        self.name=name
+        self.age=age
+        self.gender=gender
+        self.facesort=sort
+    def display(self):
+        print(self.name)
+        print(self.age)
+        print(self.gender)
+        print(self.facesort)
+zhangsan=information('zhangsan',19,'female',67)
+zhangsan.display()
