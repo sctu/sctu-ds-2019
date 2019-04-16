2166897ce49869a9d39b96c15a61b89e6d620c76
wenkangxi <2441878030@qq.com>
2019-04-16 11:08:28 +0800

    实验2-3

---
+++ 1806101076温康爔/lab02/test04.py
@@ -0,0 +1,14 @@
+# 学生：姓名张三，年龄18，性别男
+
+name:'zhangsan'
+age:18
+gender:male
+
+
+# 构造函数
+def __init__(self, name, age, gender):
+    self.name = name
+    self.age = age
+    self.fender = gender
+
+def
