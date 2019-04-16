2615353b4f39a31beee45ef94cf571331e0b2db8
vickey-zhang <1355353928@qq.com>
2019-04-16 09:06:48 +0800

    vickey's 實驗二

---
+++ 1806101046张祯悦/lab02/lab02(2).py
@@ -0,0 +1,24 @@
+#2、编程实现括号匹配。输入：{[()]} 输出：True
+str=input()
+match={'[':']','(':')','{':'}'}
+stack=[]
+status=0
+for i in str:
+    if i in match.keys():
+        stack.append(i)
+    elif i in match.values():
+        if len(stack)!=0 :
+            if match[stack[-1]]==i:
+                stack.pop()
+            else:
+                status=1
+                break
+
+        else:
+            status=1
+            break
+if status==0:
+    print('True')
+else:
+    print('False')
+
