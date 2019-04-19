4a89f32528d552426ecfa6e79739533838771fa0
wanghaijinghhh <1245476166@qq.com>
2019-04-19 09:46:22 +0800

    实验二

--- 1806101058王海静/day20190412/stack.py
+++ 1806101058王海静/day20190412/stack.py
@@ -1,37 +1,18 @@
 stack=[]
 
-#压栈
 stack.append(1)
 stack.append(2)
 stack.append(3)
-#...
-stack.append(10)
 
 for i in range(1,11):
     stack.append(i)
 
-# for i in 'hello':
-# print(i)
-
- #ctrl+d复制光标所在行
- #ctrl+x删除光标所在行
- a=stack.pop()
- print(a)
 
 
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
 
- stack.pop()
- stack.pop()
- stack.pop()
- stack.pop()
-
- #代码重复
-
- #当栈不为空的时候，一直pop()
- while len(stack) is not 0 :
-     a =stack.pop()
-     print(a)
-
-     s='hello world'
-     r=[::-1]
-     print(r)
+while len(stack) is not 0:
+    print(stack.pop())
