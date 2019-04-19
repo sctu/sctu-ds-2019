d1cc5c5854b0391bc4ea95e159f4c2b6a1b366c9
Tanmingxian <1102253310@qq.com>
2019-04-19 11:49:13 +0800

    括号匹配

--- 1806101085谈明贤/day20190419/test02.py
+++ 1806101085谈明贤/day20190419/test02.py
@@ -1,2 +1,17 @@
-def check_parens(text):
-    parens="(){}[]"
+stack=[]
+left=["{","[","("]
+right=[")","]","}"]
+for i in "{[()]}":    #对字符串遍历
+    if i in left:
+        stack.append(i)
+    if i in right:
+        left_parentheses=stack.pop()
+        a=left.index(left_parentheses)
+        b=right.index(i)
+    if a!=b:
+        print("不匹配")
+        break
+if len(stack) is 0:
+    print("匹配")
+else:
+    print("不匹配")
