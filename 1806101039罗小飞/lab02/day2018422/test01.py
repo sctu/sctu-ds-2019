da66e68b6a5252f72375dcbaf0b80d836ea2e454
luoxiaofeizhensao <1397141894@qq.com>
2019-04-22 10:38:11 +0800

    类的使用

---
+++ 1806101039罗小飞/lab02/day2018422/test01.py
@@ -0,0 +1,29 @@
+# name='justin'
+# age=18
+# gender='male'
+
+
+# class Student:
+#     # 构造函数
+#     def __init__(self, name, age, gender):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+#
+#     def study(self):
+#         print(self.name + 'studying!')
+
+
+# chen = Student('chen', '18', 'male')
+# print(chen.name)
+# print(chen.gender)
+# print(chen.age)
+
+class Girl:
+    def __init__(self, weight, stature):
+        self.weight = weight
+        self.stature = stature
+
+
+w = Girl('45', '170')
+print(w.stature)
