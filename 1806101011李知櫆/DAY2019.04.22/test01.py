a617e3d009c4c68b178e12e79dc9d82a8fe9a8d7
潘潘 <205396361@qq..com>
2019-04-22 12:11:06 +0800

    我的第一次提交

--- 1806101011李知櫆/DAY2019.04.22/test01.py
+++ 1806101011李知櫆/DAY2019.04.22/test01.py
@@ -2,7 +2,7 @@ def _init_(self,name,age,gender):
     self.name=name
     self.age=age
     self.gender =gender
-chen=student('chen,'18'male')
+chen = ('chen,' male')
 print(chen.name)
 print(chen.age)
 print(chen.gender)
