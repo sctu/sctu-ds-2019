261656d124c108b881f9e77630973d052c3ac866
fengjiejie1 <1759361922@qq.com>
2019-04-22 10:27:16 +0800

    2019.4.16课堂练习

---
+++ 1806101009冯杰/2019.04.22/练习一.py
@@ -0,0 +1,13 @@
+class Student:
+
+    #构造函数
+    #类里面的函数，第一个参数都是self
+    def __init__(self,name,'段位',):
+        self.name = name
+        self.age = 段位
+
+
+
+
+feng = Student('feng', 王者, )
+feng.'段位'()
