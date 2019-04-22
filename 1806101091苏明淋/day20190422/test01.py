236f82a14a8ba983b6461afb15beadadfaab2567
suminglin <suminglin@outlook.com>
2019-04-22 10:27:58 +0800

    更新

--- 1806101091苏明淋/day20190422/test01.py
+++ 1806101091苏明淋/day20190422/test01.py
@@ -1,5 +1,7 @@
-# 定义变量保存学生的姓名，年龄，性别
 # -*- coding: utf-8 -*-
+# 定义变量保存学生的姓名，年龄，性别
+
+
 class Student:
 
     def __init__(self,name,age,gender):
@@ -9,6 +11,12 @@ class Student:
 
     def study(self):
         print(self.name + 'studying!')
+        print(self.age)
+        print(self.gender)
 
-chen = Student('chen',18,'male')
+
+chen = Student('chen', 18, 'male')
+Tom = Student('Tom', 20, 'male')
+
 chen.study()
+Tom.study()
