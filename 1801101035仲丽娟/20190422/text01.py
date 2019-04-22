bc462f77a75cee42c6222071a4b1aaa45ed0e6df
zhonglijuan <1014343027@qq.com>
2019-04-22 10:08:12 +0800

    text01

---
+++ 1801101035仲丽娟/20190422/text01.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+#定义变量保存学生的姓名，年龄，性别
+
+name = 'justin'
+age = 18
+gender = 'male'
+
+a = 1
+b = 2
+c = 3
+
+
+class Student:
+#构造函数 __init__(self):
+#类里面写的函数，第一个参数都是self
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    def study(self):
+        print(self.name + ' studying!')
+
+
+chen = Student('chen', 18, 'male')
+chen.study()
+#print(chen.name)
+#print(chen.age)
+#print(chen.gender)
