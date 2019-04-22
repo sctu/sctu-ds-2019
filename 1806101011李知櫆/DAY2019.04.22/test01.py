90a7b82d26835fad1fb92e0836c92ad8d6c31f64
潘潘 <205396361@qq..com>
2019-04-22 10:28:36 +0800

    我的第一次提交

---
+++ 1806101011李知櫆/DAY2019.04.22/test01.py
@@ -0,0 +1,8 @@
+def _init_(self,name,age,gender):
+    self.name=name
+    self.age=age
+    self.gender =gender
+chen=student('chen,'18'male')
+print(chen.name)
+print(chen.age)
+print(chen.gender)
