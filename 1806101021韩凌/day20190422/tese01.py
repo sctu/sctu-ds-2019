5e13e4828a6033cfe2ae3395d4fe1c7429d035b6
hanling2019 <605419140@qq.com>
2019-04-22 10:10:39 +0800

    类 20190422 test 01

---
+++ 1806101021韩凌/day20190422/tese01.py
@@ -0,0 +1,19 @@
+Class Student:
+    #构造函数
+    #类里面写的函数，第一个参数都是self
+
+   def _init_(self,name,age,gender):
+       self.name = name
+       self.age = age
+       self.gender = gender
+
+    def _study(self):
+        print(self.name+'studying')
+
+
+
+chen = Student('chen','18','male')
+print(chen,name)
+print(chen.age)
+print(chen.gender)
+
