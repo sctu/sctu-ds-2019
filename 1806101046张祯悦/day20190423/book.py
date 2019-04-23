624f3edaf3713d596963a769ea3365533c2b9496
vickey-zhang <1355353928@qq.com>
2019-04-23 10:16:00 +0800

    vickey's book

---
+++ 1806101046张祯悦/day20190423/book.py
@@ -0,0 +1,13 @@
+class book
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def read(self):
+        print('read')
+
+
+Cinderella = book('disney', 27)
+Cinderella = read()
+
+
