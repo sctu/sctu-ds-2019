c44a1bc59059d13ddcda1cb5eabacad58ebb32c4
yangxi0118 <1751194165@qq.com>
2019-04-23 10:16:40 +0800

    practice

---
+++ 1806101016 杨曦/190423/02.py
@@ -0,0 +1,15 @@
+class Person:
+
+    def __init__(self , name , age):
+        self.name = name
+        self.age = age
+
+    def run(self):               #函数一般是动词开头
+        print('runing!')
+        #抽象的方法是不合理的。
+        #必须符合客观世界规律
+    def fly(self):
+        print('flying')
+
+justin = Perosn('yang',19)
+justin .run
