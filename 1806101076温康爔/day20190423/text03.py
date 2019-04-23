9a8ecdb4e724ed20e36e5cbc1d07e75f47225311
wenkangxi <2441878030@qq.com>
2019-04-23 11:40:13 +0800

    10个结点

--- 1806101076温康爔/day20190423/text03.py
+++ 1806101076温康爔/day20190423/text03.py
@@ -6,13 +6,19 @@ class Node:
         self.data = data
         self.next = None
 
-# 头结点
+# 头结点(尾插法)
 head = Node(-1)
 tail = head
 
 for i in range(1, 11):
+
+    # (1) 创建一个新的结点
     new_node = Node(i)
+
+    # (2) 将当前等链表的最后一个结点（尾结点）
     tail.next = new_node
+
+    # (3) 更新尾结点的值
     tail = new_node
 
 
