9a3026bf7afce89ff58f3e0bb1c0dce7273d9751
chengongsuo <2547253080@qq.com>
2019-04-23 11:39:05 +0800

    node.

--- 1001陈功锁/day20190423/test03.py
+++ 1001陈功锁/day20190423/test03.py
@@ -1,20 +1,20 @@
-# 1. 抽象一个结点类
+# 10个结点的单链表，值为1,2,...，10
 class Node:
 
     def __init__(self, data):
         self.data = data
         self.next = None
 
-# 2. 有三个结点
+
 n1 = Node(1)
 n2 = Node(2)
 n3 = Node(3)
+# ...
+n10 = Node(10)
 
-n = Node(4)
-
-# 3. 表达每一个结点之间的关系
-# n1的下一个结点是n2
 n1.next = n2
-
-# n2的下一个结点是n3
 n2.next = n3
+# ...
+
+
+
