2b2fb5af442e9df5405ab9a1065feea9ba52861c
zhonglijuan <1014343027@qq.com>
2019-06-03 11:41:42 +0800

    单链表

--- 1801101035仲丽娟/20190603/node.py
+++ 1801101035仲丽娟/20190603/node.py
@@ -1,14 +1,18 @@
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.next = None
 
+
 class List:
+
     def __init__(self):
-#头结点
+        # 头节点
+
         self.head = Node(-1)
-#前插法创建单链表
-    def insert_before(self,data):
+    #  前插法创建单链表
+
+    def insert_before(self, data):
         for i in data:
             node = Node(i)
 
@@ -17,55 +21,59 @@ class List:
             else:
                 node.next = self.head.next
                 self.head.next = node
-        #尾插法创建单链表
-        def insert_tail(self, data):
-            tail = self.head.next
-            for i in data:
-                node = Node(i)
+    # 尾插法创建单链表
 
-                if tail is None:
-                    self.head.next = node
-                    tail = node
-                else:
-                    tail.next = node
-                    tail = node
-        # 打印单链表
-        def list_print(self):
-            node = self.head.next
+    def insert_tail(self, data):
+        tail = self.head.next
+        for i in data:
+            node = Node(i)
 
-            while node:
-                print(node.value)
-                node = node.next
+            if tail is None:
+                self.head.next = node
+                tail = node
+            else:
+                tail.next = node
+                tail = node
+    # 打印单链表
 
-#清空链表
-def list_print(self):
-    self.head.next = None
+    def list_print(self):
+        node = self.head.next
 
-#第i个节点前插入值value的节点
-def list_element_add(self, i, value):
-    node_new = Node(value)
-    index = 0
-    node = self.head.next
-    while node:
-        index= index+1
+        while node:
+            print(node.value)
+            node = node.next
 
-        if index == i-1:
-            break
+    # 清空链表
+    def list_clear(self):
+        self.head.next = None
 
-        node = node.next
+    # 第i个节点前插入值value的节点
+    def list_element_add(self, i, value):
 
-    if node is None:
-        return False
-    node_new.next = node.next
-    node.next = node_new
+        node_new = Node(value)
+        index = 0
+        node = self.head.next
+        while node:
+            index = index+1
 
-if __name__== '_main_':
+            if index == i-1:
+                break
+
+            node = node.next
+
+        if node is None:
+            return False
+
+        node_new.next = node.next
+        node.next = node_new
+
+
+if __name__ == '_main_':
     my_list = List()
 
     my_list.insert_before([1, 2, 3, 4, 5])
 
-
-    my_list.list_clear ()
+    my_list.list_clear()
 
     my_list.insert_tail([1, 2, 3, 4, 5])
     my_list.list_print()
