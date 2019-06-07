d7b628ed6690ee4478a4808503cdac4f78e32502
SpringRiver <1874260553@qq.com>
2019-06-07 22:41:51 +0800

    第四次提交代码

--- 1801101035仲丽娟/20190603/node.py
+++ 1801101035仲丽娟/20190603/node.py
@@ -1,47 +1,10 @@
-class Node:
-    def __init__(self,value):
-        self.value = value
-        self.next = None
 
-class List:
-    def __init__(self):
-#头结点
-        self.head = Node(-1)
-#前插法创建单链表
-    def insert_before(self,data):
-        for i in data:
-            node = Node(i)
 
-            if self.head.next is None:
-                self.head.next = node
-            else:
-                node.next = self.head.next
-                self.head.next = node
-        #尾插法创建单链表
-        def insert_tail(self, data):
-            tail = self.head.next
-            for i in data:
-                node = Node(i)
 
-                if tail is None:
-                    self.head.next = node
-                    tail = node
-                else:
-                    tail.next = node
-                    tail = node
-        # 打印单链表
-        def list_print(self):
-            node = self.head.next
 
-            while node:
-                print(node.value)
-                node = node.next
 
-#清空链表
-def list_print(self):
-    self.head.next = None
 
-#第i个节点前插入值value的节点
+
 def list_element_add(self, i, value):
     node_new = Node(value)
     index = 0
