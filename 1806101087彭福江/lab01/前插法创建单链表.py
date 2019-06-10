08f01836397e051832df51a2f9dcfd9d2cf63a4a
pengfujiang <2232376344@qq.com>
2019-06-10 15:50:00 +0800

    lab01

--- 1806101087彭福江/lab01/前插法创建单链表.py
+++ 1806101087彭福江/lab01/前插法创建单链表.py
@@ -19,3 +19,16 @@ class List:
                     self.head.next = node
                 else:
                     node.head.next = node
+
+        if __name__ == '__main__':
+            my_list = List()
+
+            my_list.insert_before([1, 2, 3, 4, 5])
+            my_list.list_print()
+
+        def list_print(self):
+            node = self.head.next
+
+            while node:
+                print(node.value)
+                node = node.next
